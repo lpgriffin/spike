73d395bf810577d1fbf2fa3a837f42c7315e3e91
10096 <1009699138@qq.com>
2021-10-06 21:10:02 -0500

    1

--- src/Components/application/application.js
+++ src/Components/application/application.js
@@ -13,7 +13,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function Copyright(props) {
+const Copyright = ({props}) => {
     return (
         <Typography variant="body2" color="text.secondary" align="center" {...props}>
             {'Copyright © '}
