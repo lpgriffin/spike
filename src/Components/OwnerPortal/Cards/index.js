0aa8972ec8af78ce25397a068ff78e5d6c5b6050
louisgrif <louisgriffin12@gmail.com>
2021-10-07 00:55:47 -0500

    frontend draft

---
+++ src/Components/OwnerPortal/Cards/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+//email = user's email
+//user = type of user; Either "owner" or "renter"
+//type = type of fetch; Either "rent", "applications", or "maintenance"
+const Cards = ({ email, type }) => (
+    //TODO: add in a <ul> element for each result of the given fetch
+    <span />
+)
+
+export default Cards;
---
+++ src/Components/OwnerPortal/index.js
@@ -0,0 +1,67 @@
+import "./style.css";
+import { useState } from "react";
+import Cards from "./Cards/index";
+
+const OwnerPortal = ({ email, onLogout }) => {
+  const [portal, setPortal] = useState("home");
+
+  return (
+    <div className="owner">
+      <span className="heading">
+        MadRentals
+        <br />
+      </span>
+      {portal === "home" ? (
+        <div>
+          <span className="subheading">
+            {email} <br />
+          </span>
+          <button className="button" onClick={() => setPortal("applications")}>
+            View Resident Applications
+          </button>
+          <button className="button" onClick={() => setPortal("maintenance")}>
+            View Maintenance Requests
+          </button>
+          <button className="button" onClick={() => setPortal("rent")}>
+            View Rent Payments
+          </button>
+          <button className="button" onClick={() => onLogout()}>
+            Logout
+          </button>
+        </div>
+      ) : portal === "applications" ? (
+        <div>
+          <span className="subheading">
+            Applications <br />
+          </span>
+          <button className="button" onClick={() => setPortal("home")}>
+            Back
+          </button>
+          <Cards email={email} user={"renter"} type={"applications"} />
+        </div>
+      ) : portal === "maintenance" ? (
+        <div>
+          <span className="subheading">
+            Maintenance <br />
+          </span>
+          <button className="button" onClick={() => setPortal("home")}>
+            Back
+          </button>
+          <Cards email={email} user={"renter"} type={"maintenance"} />
+        </div>
+      ) : (
+        <div>
+          <span className="subheading">
+            Rent <br />
+          </span>
+          <button className="button" onClick={() => setPortal("home")}>
+            Back
+          </button>
+          <Cards email={email} user={"renter"} type={"rent"} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default OwnerPortal;
